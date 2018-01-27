4cac3ac45d55c54fc1b07d7550166c9a6eb5e5ab
SBuchas <sdc.darth.irule@gmail.com>
2018-01-27 10:03:13 -0500

    Add files via upload

---
+++ Grabable.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabable : MonoBehaviour {
+    GameObject mainCamera;
+    bool carrying;
+    GameObject carriedObject;
+    public float distance;
+    public float smooth;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (carrying)
+        {
+            carry(carriedObject);
+            checkDrop();
+        }
+        else
+        {
+            pickup();
+        }
+	}
+
+    void carry(GameObject carryObject)
+    {
+        carryObject.transform.position = Vector3.Lerp (carryObject.transform.position, mainCamera.transform.position + mainCamera.transform.forward * distance, Time.deltaTime * smooth);
+        carriedObject.transform.rotation = mainCamera.transform.rotation;
+    }
+
+    void pickup()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            int x = Screen.width / 2;
+            int y = Screen.height / 2;
+
+            Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Grabable p = hit.collider.GetComponent<Grabable>();
+                if (p != null)
+                {
+                    carrying = true;
+                    carriedObject = p.gameObject;
+                    p.GetComponent<Rigidbody>().isKinematic = true;
+                }
+            }
+        }
+    }
+
+    void checkDrop()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            dropItem();
+        }
+    }
+
+    void dropItem()
+    {
+        carrying = false;
+        carriedObject.GetComponent<Rigidbody>().isKinematic = false;
+        carriedObject = null;
+    }
+}
